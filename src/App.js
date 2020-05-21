2cf1a306b1544517576ef9c88e8b2e0b749bb3a9
mattcbodily <mattcbodily@gmail.com>
2020-05-21 09:29:46 -0600

    added the Header component, added styling to it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './Components/Header/Header';
 import routes from './routes';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       {routes}
     </div>
   );
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import menuIcon from '../../assets/menu.svg';
 import './Header.css';
 
 export default props => (
     <div className='header'>
-
+        <h1>Name Here</h1>
+        <img src={menuIcon} alt='hamburger menu'/>
     </div>
 )
